1f1c7bac0843e6eb7740279aa829fd28a005b652
Siddharth <sidpro@vpnduopool-10-251-133-205.near.illinois.edu>
2025-10-10 12:08:22 -0500

    Updated the Lirabry Location

--- docker/start-client.sh
+++ docker/start-client.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
-
 export DISPLAY=:0
 export LD_LIBRARY_PATH=/opt/SwarmMap/lib:/opt/SwarmMap/code/Thirdparty/DBoW2/lib:/opt/SwarmMap/code/Thirdparty/g2o/lib:/usr/local/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64
-
-# Wait for server to be ready
 sleep 3
-
 cd /opt/SwarmMap
 exec ./bin/swarm_client -v code/Vocabulary/ORBvoc.bin -d config/client.yaml -l info --viewer 0 --mapviewer 0
